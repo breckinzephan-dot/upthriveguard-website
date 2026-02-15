e7460855cb1757e3bb7535b5afdf4f3f19c19047
Breckin Mark <breckinzephan@gmail.com>
2026-02-15 21:27:57 +0000

    Import fileURLToPath and define path variables
    Add fileURLToPath and define __filename and __dirname.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { fileURLToPath } from "url";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
