822f8e062c2bc849acd4b94d1aa0a0ca3094d05c
Breckin Mark <breckinzephan@gmail.com>
2026-04-01 14:52:47 +0100

    Add files via upload

---
+++ Landing-Page-Builder/client/src/App.tsx
@@ -0,0 +1,30 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import Home from "@/pages/home";
+import SolutionDetail from "@/pages/solution-detail";
+import Gallery from "@/pages/gallery";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/solution/:id" component={SolutionDetail} />
+      <Route path="/gallery" component={Gallery} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <Router />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
